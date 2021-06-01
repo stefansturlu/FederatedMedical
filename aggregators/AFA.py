Updated the delta amplification tests with normalisation fix

aggregators/AFA.py
@@ -9,7 +9,6 @@ import copy
 from aggregators.Aggregator import Aggregator
 from torch import nn, Tensor
 from scipy.stats import beta
-import scipy
 from datasetLoaders.DatasetInterface import DatasetInterface
 
 # ADAPTIVE FEDERATED AVERAGING
@@ -93,6 +92,7 @@ class AFAAggregator(Aggregator):
 
     def aggregate(self, clients: List[Client], models: List[nn.Module]) -> nn.Module:
         empty_model = copy.deepcopy(self.model)
+        self.renormalise_weights(clients)
 
         badCount: int = 2
         slack = self.xi
@@ -172,11 +172,11 @@ class AFAAggregator(Aggregator):
         for client in clients:
             if self.notBlockedNorBadUpdate(client):
                 client.pEpoch = client.n * client.score
-                pT_epoch = pT_epoch + client.pEpoch
+                pT_epoch += client.pEpoch
 
         for client in clients:
             if self.notBlockedNorBadUpdate(client):
-                client.pEpoch = client.pEpoch / pT_epoch
+                client.pEpoch /= pT_epoch
         # logPrint("Updated scores:{}".format([client.pEpoch for client in clients]))
         comb = 0.0
         for i, client in enumerate(clients):

aggregators/FedAvg.py
@@ -37,7 +37,7 @@ class FAAggregator(Aggregator):
 
     def aggregate(self, clients: List[Client], models: List[nn.Module]) -> nn.Module:
         empty_model = deepcopy(self.model)
-        # TODO: RENORMALISE THE WEIGHTS FOR PRIV AMP
+        self.renormalise_weights(clients)
 
         comb = 0.0
         for i, client in enumerate(clients):

experiment/CustomConfig.py
@@ -1,4 +1,4 @@
-from utils.typings import AttacksType
+from utils.typings import AttacksType, FreeRiderAttack
 from aggregators.Aggregator import allAggregators
 from aggregators.GroupWise import GroupWiseAggregation
 from typing import List
@@ -40,7 +40,10 @@ class CustomConfig(DefaultExperimentConfiguration):
         ]
         self.percUsers = torch.tensor(PERC_USERS, device=self.aggregatorConfig.device)
 
-        self.aggregators = [FAAggregator, COMEDAggregator, MKRUMAggregator]
+        self.aggregators = [FAAggregator, COMEDAggregator, MKRUMAggregator, AFAAggregator]
+        self.aggregatorConfig.privacyAmplification = True
+        self.aggregatorConfig.freeRiderAttack = FreeRiderAttack.DELTA
+        self.aggregatorConfig.detectFreeRiders = True
 
     def scenario_conversion(self):
         for faulty, malicious, freeRider, attackName in self.scenarios: